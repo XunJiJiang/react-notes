ba8a91472ff5e552a411e89d506ccd405a94959b
xunjijiang <329453626x@gmail.com>
2024-03-22 19:51:24 +0800

    feat: 修改 获取目录的最深层级函数的声明位置

--- src/components/contents/index.js
+++ src/components/contents/index.js
@@ -1,36 +1,35 @@
 import './index.css';
 import Content from './components/content';
 
-export default function Contents ({ title = '目录', contents = [], onChange = () => {} }) {
-  /**
-   * 计算目录的宽度
-   */
-  const width = ((() => {
-    /**
-     * 获取目录的最深层级
-     * @param {{
-     *   component: any | null | undefined,
-     *   children: contents | undefined,
-     *   label: string,
-     *   tag: string | undefined
-     * }[]} contents
-     * @returns 
-     */
-    function getDeepestLayer (contents) {
-      let layer = 1;
-      const childLayers = [];
-      contents.forEach((content) => {
-        if (Array.isArray(content.children)) {
-          childLayers.push(getDeepestLayer(content.children));
-        } else {
-          return layer;
-        }
-      });
-      if (childLayers.length > 0) {
-        layer = Math.max(...childLayers) + layer;
-      }
+/**
+ * 获取目录的最深层级
+ * @param {{
+ *   component: any | null | undefined,
+ *   children: contents | undefined,
+ *   label: string,
+ *   tag: string | undefined
+ * }[]} contents
+ * @returns 
+ */
+function getDeepestLayer (contents) {
+  let layer = 1;
+  const childLayers = [];
+  contents.forEach((content) => {
+    if (Array.isArray(content.children)) {
+      childLayers.push(getDeepestLayer(content.children));
+    } else {
       return layer;
     }
+  });
+  if (childLayers.length > 0) {
+    layer = Math.max(...childLayers) + layer;
+  }
+  return layer;
+}
+
+export default function Contents ({ title = '目录', contents = [], onChange = () => {} }) {
+  /** 目录的宽度 */
+  const width = ((() => {
     // 每深一层，增加28px
     return 150 + 44 + 28 * getDeepestLayer(contents) - 1 + 'px';
   })());
