76543b1318fdcccdd18f7332f7c415e1f353a317
xunjijiang <329453626x@gmail.com>
2024-03-23 21:06:31 +0800

    feat：当菜单重复点击同一个页面时，不会重加载此页面

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import './index.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import contents from './contents.js';
 import Contents from './contents/index.js';
 import Transition from './transition/index.js';
@@ -8,6 +8,8 @@ export default function Components () {
   // 当前展示的组件
   const [mainNode, setMainNode] = useState(null);
 
+  const lastContext = useRef(null);
+
   /**
    * 组件切换事件处理
    * @param {object} context
@@ -15,7 +17,9 @@ export default function Components () {
    * @param {object} context.component 当前展示的组件
    */
   function changeHandler (context) {
+    if (context.content === lastContext.current) return;
     setMainNode(context.component);
+    lastContext.current = context.content;
   }
 
   return (
