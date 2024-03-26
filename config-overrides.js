0edee088988ed4bbccff417589514aabe26692c1
xunjijiang <329453626x@gmail.com>
2024-03-26 17:43:02 +0800

    feat: 引入raw-loader

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+// config-overrides.js
+
+const { override, addWebpackModuleRule } = require('customize-cra');
+
+module.exports = override(
+  addWebpackModuleRule({
+    test: /\.md$/,
+    use: 'raw-loader',
+  })
+);
