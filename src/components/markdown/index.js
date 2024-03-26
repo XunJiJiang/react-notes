452b02b2efb036966466d242898ca8c370e32561
xunjijiang <329453626x@gmail.com>
2024-03-26 17:46:26 +0800

    feat: markdown 组件 ref 添加1、2级目录的上下文

--- src/components/markdown/index.js
+++ src/components/markdown/index.js
@@ -1,16 +1,24 @@
+import './index.css';
+import { forwardRef, useImperativeHandle } from 'react';
 import Markdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+// import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import remarkGfm from 'remark-gfm';
 
-export default function MarkdownComponent ({ markdown }) {
+function MarkdownComponent ({ markdown }, ref) {
+  const contents = new Map();
+
+  useImperativeHandle(ref, () => ({
+    contents
+  }));
+
   return (
     <Markdown
       children={markdown}
       remarkPlugins={[remarkGfm]}
       components={{
         code ({ node, inline, className, children, ...props }) {
-          const match = /language-(\w+)/.exec(className || '')
+          const match = /language-(\w+)/.exec(className ?? '');
           return !inline && match ? (
             <SyntaxHighlighter
               children={String(children).replace(/\n$/, '')}
@@ -20,12 +28,57 @@ export default function MarkdownComponent ({ markdown }) {
               {...props}
             />
           ) : (
-            <code className={className} {...props}>
+            <code className={`markdown-short-code ${className ?? ''}`} {...props}>
               {children}
             </code>
           )
-        }
+        },
+        a ({ node, inline, className, children, ...props }) {
+          return (
+            <a className={`markdown-link ${className ?? ''}`} {...props}>
+              {children}
+            </a>
+          )
+        },
+        h1 ({ node, inline, className, children, ...props }) {
+          const id = children.split(' ').join('-').toLowerCase();
+          contents.set(id, {
+            label: children,
+            level: 1,
+            node: null,
+          });
+          return (
+            <h1
+              ref={(node) => {
+                contents.get(id).node = node;
+              }}
+              className={`markdown-h1 ${className ?? ''}`} {...props}
+            >
+              {children}
+            </h1>
+          )
+        },
+        h2 ({ node, inline, className, children, ...props }) {
+          const id = children.split(' ').join('-').toLowerCase();
+          contents.set(id, {
+            label: children,
+            level: 2,
+            node: null,
+          });
+          return (
+            <h2
+              ref={(node) => {
+                contents.get(id).node = node;
+              }}
+              className={`markdown-h2 ${className ?? ''}`} {...props}
+            >
+              {children}
+            </h2>
+          )
+        },
       }}
     />
   )
 }
+
+export default forwardRef(MarkdownComponent);
--- src/components/page-template/index.js
+++ src/components/page-template/index.js
@@ -1,11 +1,18 @@
 import './index.css';
+import { useEffect, useRef } from 'react';
 import MarkdownComponent from '../markdown';
 
 export default function PageTemplate ({ children, markdown }) {
+  const ref = useRef(null);
+  useEffect(() => {
+  }, []);
   return (
     <div className='page-template'>
       <div className='page-template-markdown'>
-        <MarkdownComponent markdown={markdown} />
+        <MarkdownComponent
+          ref={ref}
+          markdown={markdown}
+        />
       </div>
       <div className='page-template-render'>
         {children}
