c25b4750e151d482322822b8a7b38e5e040abde3
xunjijiang <329453626x@gmail.com>
2024-03-29 22:15:47 +0800

    feat：添加页面内目录位置

--- src/components/page-template/index.js
+++ src/components/page-template/index.js
@@ -1,9 +1,13 @@
 import './index.css';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import MarkdownComponent from '../markdown';
+import Contents from './components/contents';
 
 export default function PageTemplate ({ children, markdown = '> 没有 markdown 文本' }) {
-  const ref = useRef(null);
+  const markdownRef = useRef(null);
+
+  const [markdownContents, setMarkdownContents] = useState([]);
+
   useEffect(() => {
     // console.log(ref.current)
   }, []);
@@ -11,12 +15,17 @@ export default function PageTemplate ({ children, markdown = '> 没有 markdown 
     <div className='page-template'>
       <div className='page-template-markdown'>
         <MarkdownComponent
-          ref={ref}
+          ref={(node) => {
+            if (node && node.contents && markdownContents.length === 0) {
+              setMarkdownContents(node.contents);
+            }
+            return markdownRef;
+          }}
           markdown={markdown}
         />
       </div>
-      <div>
-        <nav></nav>
+      <div className='page-template-contents'>
+        <Contents contents={markdownContents} />
       </div>
       {children && <div className='page-template-render'>
         {children}
