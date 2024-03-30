09157a1cd9c8ffa65678e8939ffd80979fc1a3d7
xunjijiang <329453626x@gmail.com>
2024-03-30 22:24:47 +0800

    feat：添加markdown目录

---
+++ src/components/page-template/components/contents/index.js
@@ -0,0 +1,70 @@
+import './index.css';
+import { useRef, forwardRef, useImperativeHandle, useEffect } from 'react';
+
+function ContentsINPage ({ contents }, ref) {
+
+  const ulRef = useRef(null);
+
+  const liList = useRef([]);
+
+  function changeLocation (content) {
+    const liNode = document.getElementById(`contents-in-page-${content.id}`);
+    const liNodes = ulRef.current.childNodes;
+    for (let i = 0; i < liNodes.length; i++) {
+      const li = liNodes[i];
+      li.setAttribute('data-active', false);
+    }
+    liNode.setAttribute('data-active', true);
+  }
+
+  useEffect(() => {
+    // changeLocation(contents[0])
+    // console.log(contents[0]);
+  });
+
+  useImperativeHandle(ref, () => ({
+    changeLocation
+  }));
+
+  return (
+    <nav className='contents-in-page'>
+      <span className='contents-in-page-title'>目录</span>
+      <ul ref={ulRef}>
+        {
+          contents.filter((content) => {
+            return content.level !== 1;
+          }).map((content, index) => {
+            return (
+              <li
+                id={`contents-in-page-${content.id}`}
+                className='contents-in-page-item-box'
+                ref={() => {
+                  liList.current.push(index);
+                }}
+                key={index}
+                onClick={() => {
+                  const id = content.id;
+                  const node = document.getElementById(id);
+                  if (id && node) {
+                    node.scrollIntoView({ behavior: 'smooth' });
+                  } else {
+                    console.warn('目录项对应的节点不存在');
+                  }
+                }}
+              >
+                <a
+                  href={`#${content.id}`}
+                  className={`contents-in-page-item contents-in-page-item-${content.level}`}
+                >
+                  {content.label}
+                </a>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </nav>
+  );
+}
+
+export default  forwardRef(ContentsINPage);
--- src/components/page-template/index.js
+++ src/components/page-template/index.js
@@ -1,35 +1,83 @@
 import './index.css';
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import MarkdownComponent from '../markdown';
 import Contents from './components/contents';
 
+let a = 1;
+
+const scrollEvent = 'scroll';
+
 export default function PageTemplate ({ children, markdown = '> 没有 markdown 文本' }) {
+
+  const scrollRef = useRef(null);
+
   const markdownRef = useRef(null);
+
+  const markdownContentsRef = useRef([]);
 
   const [markdownContents, setMarkdownContents] = useState([]);
 
-  useEffect(() => {
-    // console.log(ref.current)
-  }, []);
+  /**
+   * 滚动事件处理
+   */
+  function scrollHandler () {
+    if (markdownContentsRef.current && markdownContentsRef.current.changeLocation) {
+      // 视口高度
+      const viewHeight = window.innerHeight;
+      const inView = [];
+      for (let i = markdownContents.length - 1; i >= 0; i--) {
+        const content = markdownContents[i]; 
+        if (content.offsetTop <= scrollRef.current.scrollTop + viewHeight / 2 && 
+          content.offsetTop >= scrollRef.current.scrollTop - 50) {
+          inView.push(content);
+        }
+        if (content.offsetTop < scrollRef.current.scrollTop - 50) {
+          if (inView.length === 0) {
+            markdownContentsRef.current.changeLocation(content);
+          } else {
+            markdownContentsRef.current.changeLocation(inView.pop());
+          }
+          break;
+        }
+      }
+    }
+  }
+
   return (
-    <div className='page-template'>
-      <div className='page-template-markdown'>
-        <MarkdownComponent
-          ref={(node) => {
-            if (node && node.contents && markdownContents.length === 0) {
-              setMarkdownContents(node.contents);
-            }
-            return markdownRef;
-          }}
-          markdown={markdown}
-        />
+    <div
+      ref={(node) => {
+        if (node) {
+          node.removeEventListener(scrollEvent, scrollHandler);
+          node.addEventListener(scrollEvent, scrollHandler);
+        }
+        scrollRef.current = node;
+      }}
+      className='page-template-box'
+      id='page-template-box'
+    >
+      <div className='page-template'>
+        <div className='page-template-markdown'>
+          <MarkdownComponent
+            ref={(node) => {
+              console.log('渲染次数', a++);
+              if (node && node.contents && markdownContents.length === 0) {
+                setMarkdownContents(node.contents);
+              }
+              return markdownRef;
+            }}
+            markdown={markdown}
+          />
+        </div>
+        <div className='page-template-contents'>
+          <Contents
+            ref={markdownContentsRef}
+            contents={markdownContents}
+          />
+        </div>
+        {children && <div className='page-template-render'>
+          {children}
+        </div>}
       </div>
-      <div className='page-template-contents'>
-        <Contents contents={markdownContents} />
-      </div>
-      {children && <div className='page-template-render'>
-        {children}
-      </div>}
     </div>
   )
 }
