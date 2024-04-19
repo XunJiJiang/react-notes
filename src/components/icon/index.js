7142c25b88f9a3013345e8247b818a558306bbfa
xunjijiang <329453626x@gmail.com>
2024-04-19 21:38:46 +0800

    feat: 修改icon字体尺寸设置位置

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -1,11 +1,11 @@
 import './index.css';
 import '@/assets/font/iconfont.css';
 
-export default function Icon ({ name = '', className, size = 16 }) {
+export default function Icon ({ name = '', className, size }) {
   return (
     <i
       className={`icon iconfont icon-${name} ${className}`}
-      style={{ fontSize: size + 'px' }}
+      style={size ? { fontSize: size + 'px' } : {}}
     />
   );
 }
