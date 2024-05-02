769ab5b059df84de5554a3aa444fb8bd5800ad08
xunjijiang <329453626x@gmail.com>
2024-05-02 19:34:53 +0800

    feat: 新增工具函数markdownImgPathFormatting
    用于将markdown中的图片语句中的路径转换为正确的图片路径

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,5 @@ export { default as getStringWidth } from './modules/getStringWidth';
 export { default as randomCharacters } from './modules/randomCharacters';
 
 export { default as deepCopy } from './modules/deepCopy';
+
+export { default as markdownImgPathFormatting } from './modules/markdownImgPathFormatting';
