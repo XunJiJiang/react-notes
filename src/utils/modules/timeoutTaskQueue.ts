7bddfb3a3995abbd293af0bdc4bafb7487753e49
xunjijiang <329453626x@gmail.com>
2024-05-24 21:22:56 +0800

    feat: TimeoutTaskQueue 函数功能更新
    重写添加任务；
    异步函数支持；
    运行和停止；
    单次运行；

--- src/utils/modules/timeoutTaskQueue.ts
+++ src/utils/modules/timeoutTaskQueue.ts
@@ -1,54 +1,185 @@
 interface Task {
-  timeout: number;
-  callback: () => void;
+  delay?: number;
+  callback: () => void | Promise<void>;
+}
+
+function isAsyncFunction<T>(
+  value: unknown,
+): value is (...args: unknown[]) => Promise<T> {
+  return (
+    typeof value === 'function' && value.constructor.name === 'AsyncFunction'
+  );
 }
 
 /**
  * 创建延时任务队列，按照添加任务的顺序执行
  */
 class TimeoutTaskQueue {
-  private promise: [Promise<void>, () => void] | [null, null] = [null, null];
+  private _promise: [Promise<void>, () => void] | [null, null] = [null, null];
 
-  private finallyCallback: (() => void) | null = null;
+  private _finallyCallback: (() => void)[] = [];
 
-  private isRan = false;
+  private _taskQueue: Task[] = [];
 
-  constructor() {
-    this.promise[0] = new Promise((resolve) => {
-      this.promise[1] = resolve;
-    });
-  }
+  private _queuedNumber = 0;
 
-  addTask({ timeout, callback }: Task): TimeoutTaskQueue {
-    const task = { timeout, callback };
-    this.promise[0] = this.promise[0]!.then(() => {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          task.callback();
-          resolve();
-        }, task.timeout);
-      });
+  private _state: 'pending' | 'complete' | 'running' = 'complete';
+
+  private _isRunning = false;
+
+  private _addTask({ delay, callback }: Task): TimeoutTaskQueue {
+    this._promise[0] = this._promise[0]!.then(() => {
+      if (isAsyncFunction<void>(callback)) {
+        if (typeof delay === 'number') {
+          return new Promise((resolve) => {
+            setTimeout(() => {
+              callback().then(resolve);
+            }, delay);
+          });
+        }
+        return callback();
+      }
+
+      if (typeof delay === 'number') {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            callback();
+            resolve();
+          }, delay);
+        });
+      }
+
+      callback();
+      return Promise.resolve();
     });
     return this;
   }
 
-  finally(callback: () => void): TimeoutTaskQueue {
-    if (this.finallyCallback) {
-      throw new Error('TimeoutTaskQueue: finally callback already exists');
-    }
-    this.finallyCallback = callback;
+  /**
+   * 添加一个任务
+   */
+  public addTask(task: Task): TimeoutTaskQueue {
+    this._taskQueue.push(task);
+
     return this;
   }
 
-  run(): void {
-    if (this.isRan) {
-      throw new Error('TimeoutTaskQueue: already ran');
-    }
-    this.promise[0]!.finally(() => {
-      this.finallyCallback?.();
-      this.isRan = true;
+  private _createTaskQueue(once: boolean): TimeoutTaskQueue {
+    this._promise[0] = new Promise((resolve) => {
+      this._promise[1] = resolve;
     });
-    this.promise[1]!();
+    this._taskQueue.forEach((task) => {
+      this._addTask(task);
+    });
+    this._promise[0]!.finally(() => {
+      this._finallyCallback.forEach((callback) => callback());
+      this._isRunning = false;
+      if (!once) {
+        this._run();
+      } else {
+        if (this._queuedNumber === 0) {
+          this._state = 'complete';
+        } else {
+          this._state = 'pending';
+        }
+      }
+    });
+    return this;
+  }
+
+  /**
+   * 添加一个 finally 回调，当队列中的任务全部执行完毕后执行
+   */
+  public finally(callback: () => void): TimeoutTaskQueue {
+    this._finallyCallback.push(callback);
+    return this;
+  }
+
+  private _run(once = false): void {
+    if (this._isRunning) {
+      return;
+    }
+    if (this._queuedNumber === 0) {
+      this._state = 'complete';
+      return;
+    }
+    if (this._state === 'pending') {
+      return;
+    }
+    this._isRunning = true;
+    this._queuedNumber -= 1;
+    this._createTaskQueue(once);
+    this._promise[1]!();
+  }
+
+  /**
+   * 执行一次任务
+   */
+  public runOnce(
+    callback: (() => void) | false = () => {
+      console.warn(
+        'TimeoutTaskQueue: This operation "runOnce" is invalid when the task is running',
+      );
+    },
+  ) {
+    if (this._state === 'running' || this._isRunning) {
+      callback && callback();
+      return;
+    }
+    this._queuedNumber += 1;
+    this._state = 'running';
+    this._run(true);
+  }
+
+  /**
+   * 添加运行任务到队列中
+   * @param times 添加任务的次数
+   */
+  public addRunQueue(times = 1) {
+    this._queuedNumber += times;
+    this._state = 'pending';
+  }
+
+  /**
+   * 开始执行任务
+   */
+  public start(): void {
+    if (this._state === 'running') {
+      console.warn(
+        'TimeoutTaskQueue: This operation "start" is invalid when the task is running',
+      );
+      return;
+    }
+    if (this._state === 'complete') {
+      return;
+    }
+    this._state = 'running';
+    this._run();
+  }
+
+  /**
+   * 暂停任务(正在运行的任务将继续运行至结束)
+   */
+  public pause(): void {
+    if (this._state !== 'complete') this._state = 'pending';
+  }
+
+  /**
+   * 任务状态
+   * @returns {'pending' | 'complete' | 'running'}
+   * @return pending: 运行队列有未完成任务, 当前已暂停(正在运行的任务将继续运行至结束)
+   * @return complete: 运行队列没有未完成任务
+   * @return running: 正在运行任务
+   */
+  public get state(): 'pending' | 'complete' | 'running' {
+    return this._state;
+  }
+
+  /**
+   * 运行队列中剩余的运行次数
+   */
+  public get remainingTimes(): number {
+    return this._queuedNumber;
   }
 }
 
