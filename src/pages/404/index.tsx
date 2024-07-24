0c95088db5c4ff27d7b88d88b456efd67e2cfa66
xunjijiang <329453626x@gmail.com>
2024-07-25 01:04:07 +0800

    feat: 修改404页面样式

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -4,7 +4,8 @@ export default function NotFound({ info = '' }: { info?: string }) {
   return (
     <div className="not-found">
       <div>
-        <h1>404 Not Found</h1>
+        <h1>哦不! </h1>
+        <p>不好意思, 这貌似是一个预期外的问题</p>
         <p>{info}</p>
       </div>
     </div>
