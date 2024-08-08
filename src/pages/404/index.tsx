5ec1a63e1d4081ee76d3e7e9e90a9169ca66048f
xunjijiang <329453626x@gmail.com>
2024-08-09 01:15:32 +0800

    feat: 新的错误页面内容

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -4,8 +4,12 @@ export default function NotFound({ info = '' }: { info?: string }) {
   return (
     <div className="not-found">
       <div>
-        <h1>哦不! </h1>
-        <p>不好意思, 这貌似是一个预期外的问题</p>
+        <h1>哦不!</h1>
+        <p>
+          {typeof info === 'undefined'
+            ? '不好意思, 这貌似是一个预期外的问题'
+            : '建议您检查下一输入的路径!'}
+        </p>
         <p>{info}</p>
       </div>
     </div>
--- src/router/error/error-page.tsx
+++ src/router/error/error-page.tsx
@@ -1,20 +1,47 @@
 import NotFound from '@pages/404/index.tsx';
-import { useEffect } from 'react';
-import { useRouteError, useLocation, useNavigate } from 'react-router-dom';
+// import { useEffect } from 'react';
+import {
+  useRouteError,
+  useLocation /*, useNavigate */
+} from 'react-router-dom';
+import { AnyMap } from '@utils/index.ts';
 
-export default function ErrorPage() {
+const errorTypeMap = new AnyMap();
+
+errorTypeMap.set('NOT_FOUND', () => {
   const location = useLocation();
-  const navigate = useNavigate();
+  return {
+    ...location,
+    message: `路径 ${location.pathname} 下没有内容`
+  };
+});
+// TODO: 预计支持注册错误类型
 
-  useEffect(() => {
-    if (location.pathname !== '/error') navigate('/error');
-  });
+function useError(type?: string) {
+  const routeError = useRouteError();
 
-  const error = (useRouteError() ?? {
+  if (errorTypeMap.has(type)) {
+    const error = errorTypeMap.get(type)();
+    return error;
+  }
+
+  return (routeError ?? {
     statusText: 'Not Found'
   }) as {
     statusText: string;
   };
+}
 
-  return <NotFound info={error!.statusText || 'Not Found'} />;
+export default function ErrorPage({ type }: { type?: string }) {
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   if (location.pathname !== '/error') navigate('/error');
+  // });
+
+  const error = useError(type);
+
+  // const errorValue = errorTypeMap.get(error.statusText);
+
+  return <NotFound info={error!.statusText || error!.message || void 0} />;
 }
