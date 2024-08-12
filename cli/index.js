81851a09fb0d5d162a28561ff679916ab76e2e2f
xunjijiang <329453626x@gmail.com>
2024-08-13 02:25:01 +0800

    chore: 优化cli调用栈结构
    返回上级目录由添加一层调用栈改为退出一层调用栈

--- cli/index.js
+++ cli/index.js
@@ -18,58 +18,63 @@ const rootPageDirectoryPath = path.resolve(__dirname, './src/pages/study');
 /** 创建和处理选择文件夹的选项 */
 async function logSelect (message, directoryPath = './src/pages/study') {
 
-  // 获取 ../src/pages/study 目录下的所有文件夹
-  const studyPath = path.resolve(__dirname, directoryPath);
+  while (true) {
+    // 获取 ../src/pages/study 目录下的所有文件夹
+    const studyPath = path.resolve(__dirname, directoryPath);
 
-  const studyDirs = getDirectory(studyPath);
+    const studyDirs = getDirectory(studyPath);
 
-  const choices = ['创建节点', '退出', '返回上一级', ...studyDirs].map((choice) => {
-    if (choice === '返回上一级' && studyPath.endsWith('\\src\\pages\\study')) {
-      return {
-        name: choice,
-        value: choice,
-        disabled: '已经是根目录了'
-      };
+    const choices = ['创建节点', '退出', '返回上一级', ...studyDirs].map((choice) => {
+      if (choice === '返回上一级' && studyPath.endsWith('\\src\\pages\\study')) {
+        return {
+          name: choice,
+          value: choice,
+          disabled: '已经是根目录了'
+        };
+      } else {
+        return {
+          name: choice,
+          value: choice
+        };
+      }
+    });
+
+    const answer = await createSelect(
+      message
+      + '\n'
+      + csl.createColor('cyan')(
+          '当前路径> root'
+          + (studyPath.replace(/\\/g, '/').split('/src/pages/study')[1] || '')
+          + '/'
+        )
+      + '\n'
+      + csl.createColor('green')('>'),
+      choices
+    );
+
+    if (answer === '创建节点') {
+      try {
+        const result = await createNode(studyPath);
+        csl.success(result);
+      } catch (error) {
+        csl.warn(error);
+      } finally {
+        return await logSelect(message, studyPath);
+      }
+    } else if (answer === '退出') {
+      csl.color('期待与您的再次相遇');
+      await updateContents(rootPageDirectoryPath);
+      execSync(`npx prettier --write ${path.resolve(__dirname, './src/contents/index.tsx')}`);
+      return '--exit';
+    } else if (answer === '返回上一级') {
+      return '--back';
+      // await logSelect(message, path.resolve(studyPath, '../'));
     } else {
-      return {
-        name: choice,
-        value: choice
-      };
+      const _return = await logSelect(message, path.resolve(studyPath, answer));
+      if (_return === '--exit') {
+        return '--exit';
+      }
     }
-  });
-
-  const answer = await createSelect(
-    message
-    + '\n'
-    + csl.createColor('cyan')(
-        '当前路径> root'
-        + (studyPath.replace(/\\/g, '/').split('/src/pages/study')[1] || '')
-        + '/'
-      )
-    + '\n'
-    + csl.createColor('green')('>'),
-    choices
-  );
-
-  
-  if (answer === '创建节点') {
-    try {
-      const result = await createNode(studyPath);
-      csl.success(result);
-    } catch (error) {
-      csl.warn(error);
-    } finally {
-      return await logSelect(message, studyPath);
-    }
-  } else if (answer === '退出') {
-    csl.color('期待与您的再次相遇');
-    await updateContents(rootPageDirectoryPath);
-    execSync(`npx prettier --write ${path.resolve(__dirname, './src/contents/index.tsx')}`);
-    return;
-  } else if (answer === '返回上一级') {
-    return await logSelect(message, path.resolve(studyPath, '../'));
-  } else {
-    return await logSelect(message, path.resolve(studyPath, answer));
   }
 }
 
