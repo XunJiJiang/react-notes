795df10e6aa87eceffaae2ecfc8074035640a914
xunjijiang <329453626x@gmail.com>
2024-08-18 00:38:35 +0800

    feat: 允许为直接载入到body的元素的载体元素添加样式

--- src/components/mount-to-body/index.tsx
+++ src/components/mount-to-body/index.tsx
@@ -4,9 +4,10 @@ import { createPortal } from 'react-dom';
 export interface MountToBodyProps {
   children: React.ReactNode;
   rootClass?: string;
+  style?: React.CSSProperties;
 }
 
-const MountToBody = ({ children, rootClass }: MountToBodyProps) => {
+const MountToBody = ({ children, rootClass, style = {} }: MountToBodyProps) => {
   const div = document.createElement('div');
 
   div.className = rootClass || '';
@@ -15,10 +16,11 @@ const MountToBody = ({ children, rootClass }: MountToBodyProps) => {
 
   useEffect(() => {
     document.body.appendChild(container);
+    Object.assign(container.style, style);
     return () => {
       document.body.removeChild(container);
     };
-  }, [container]);
+  }, [container, style]);
 
   return createPortal(children, container);
 };
