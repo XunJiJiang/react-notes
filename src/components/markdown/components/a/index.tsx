07178d47cf9536572ca765125c66f85b185a866d
xunjijiang <329453626x@gmail.com>
2024-09-06 22:55:38 +0800

    feat(markdown): 添加对相对路径的解析

--- src/components/markdown/components/a/index.tsx
+++ src/components/markdown/components/a/index.tsx
@@ -1,7 +1,7 @@
 import type { AnchorPopoverProps } from '@type/modules/comp-markdown-comp-a.d.ts';
 
 import './index.css';
-import { useRef, useState, useContext } from 'react';
+import { useRef, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Icon from '@components/icon/index.tsx';
 import Tooltip from '@/components/tooltip/index.tsx';
@@ -16,23 +16,24 @@ const AnchorPopover = ({
 
   const aRef = useRef<HTMLAnchorElement>(null);
 
-  const isHover = useRef(false);
-  const isFocus = useRef(false);
-
-  const [, /* isVisible, */ setIsVis] = useState(false);
-
-  const updateVisible = () => {
-    setIsVis(isHover.current || isFocus.current);
-  };
-
   // const { origin: hrefOrigin, pathname, search, hash } = new URL(href);
 
-  const isFromThisSite = href.startsWith(origin);
+  const isFromThisSite =
+    href.startsWith(origin) ||
+    href.startsWith('/') ||
+    href.startsWith('#') ||
+    href.startsWith('.');
+
+  const fullPath = href.startsWith('/')
+    ? new URL('.' + href, origin).href
+    : href.startsWith('.')
+      ? new URL(href, origin).href
+      : href;
 
   return (
     <>
       <Tooltip
-        title={decodeURI(href)}
+        title={decodeURI(fullPath)}
         backgroundColor="black"
         color="white"
         placement="top"
@@ -40,29 +41,12 @@ const AnchorPopover = ({
         trigger={['hover', 'focus']}
         showAfter={300}
         hideAfter={50}
-        // visible={isVisible}
       >
         <Link
           ref={aRef}
           className={`${className} a-link`}
-          to={href}
+          to={fullPath}
           reloadDocument={!isFromThisSite}
-          onMouseEnter={() => {
-            isHover.current = true;
-            updateVisible();
-          }}
-          onMouseLeave={() => {
-            isHover.current = false;
-            updateVisible();
-          }}
-          onFocus={() => {
-            isFocus.current = true;
-            updateVisible();
-          }}
-          onBlur={() => {
-            isFocus.current = false;
-            updateVisible();
-          }}
         >
           <span className="a-link-content">
             {children}
